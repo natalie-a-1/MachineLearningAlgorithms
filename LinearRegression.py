42cac75c7856c60ce336de6933ec40d9592179c2
nataliehill <natalie.a.hill-1@ou.edu>
2022-08-26 08:57:36 -0500

    LinearRegresssion.py predicts y-value based on X_text value

--- LinearRegression.py
+++ LinearRegression.py
@@ -8,23 +8,21 @@ plt.style.use("seaborn-dark")
 
 data = pd.read_csv('DataSets/score.csv')
 
+plt.figure(figsize=(5, 5))
+plt.xlabel("Hours")
+plt.ylabel("Scores")
+plt.title("Student's Scores and Hours of Studying")
+for i in range(0, data.shape[0]):
+    plt.scatter(data['Hours'][i], data['Scores'][i], color='blue')
 
-def CreateVisuals(dataFrame):
-    plt.figure(figsize=(10, 10))
-    plt.xlabel("Hours")
-    plt.ylabel("Scores")
-    plt.title("Student's Scores and Hours of Studying")
-    for i in range(0, data.shape[0]):
-        plt.scatter(data['Hours'][i], data['Scores'][i], color='Green')
-    plt.plot()
-    return plt
-
-
-def Init(dataFrame):
-    plot = CreateVisuals(data)
-    plot.show()
-
-# def TrainandTest(DataFrame):
-#     X, y = data['Hours'], data['Scores']
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, test_size=1/4, random_state=0)
+x = data['Hours'].values.reshape(-1, 1)
+y = data['Scores'].values.reshape(-1, 1)
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=1/4, random_state=0)
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+LinearRegression(copy_X=True, fit_intercept=True,
+                 n_jobs=1, normalize=False)
+y_results = lr.predict(X_test)
+plt.plot(X_test, y_results, color='red')
+plt.show()
